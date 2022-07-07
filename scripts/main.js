0978e612ee7567c79963c975ec54aa186055d1c5
NinJiu9 <95898218+NinJiu9@users.noreply.github.com>
2022-07-07 16:45:28 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+//let myHeading=document.querySelector('h1');
+//myHeading.textContent='Hello World';
+//document.querySelector('h1').onclick = function() {
+//    alert('别戳我，我怕疼。');
+//}
+let myImage= document.querySelector('img');
+
+myImage.onclick=function(){
+	let mySrc=myImage.getAttribute('src');
+	if(mySrc==='img/1.jpg'){
+		myImage.setAttribute('src','img/2.jpg');
+	}else{
+		myImage.setAttribute('src','img/1.jpg');
+	}
+	
+}
+
+let myButton =document.querySelector('button');
+let myHeading=document.querySelector('h2');
+
+function setUserName(){
+	let myName=prompt('请输入你的名字');
+	if(!myName){
+		setUserName();
+	}else{
+		localStorage.setItem('name',myName);
+		myHeading.textContent='Mozilla,'+myName;
+	}
+	//localStorage.setItem('name',myName);
+	//myHeading.textContent='Mozilla,'+myName;
+}
+
+myButton.onclick=function(){
+	//alert('切换');
+	setUserName();
+}
+
+if(!localStorage.getItem('name')){
+	setUseName();
+}else{
+	let storedName=localStorage.getItem('name');
+	myHeading.textContent='Mozilla,'+storedName;
+}
+
+
